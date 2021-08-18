770ed75046dd7b149ae00f4caa0a575e87452fc3
maoger <maoyantsing@gmail.com>
2021-08-18 17:24:14 +0800

    update V0.1.16
    优化：自动替换“题库”中的某些非标字符，如：英文逗号 `,`、换行符`\r\n`

--- full/full_0.1.16.js
+++ full/full_0.1.16.js
@@ -1,26 +1,13 @@
-// ==UserScript==
-// @name         EducationEnhancer
-// @icon         http://ce.esnai.net/favicon.ico
-// @homepage     https://github.com/maoger/EducationEnhancer
-// @version      0.1.16
-// @description  网课学习助手
-// @author       Maoger
-// @match        http*://*.esnai.net/*
-// @match        http*://wangda.chinamobile.com/*
-// @grant        none
-// @run-at       document-idle
-// @updateURL    https://openuserjs.org/meta/maoger/EducationEnhancer.meta.js
-// @copyright    2021-2021, maoger (https://openuserjs.org/users/maoger)
-// @license      MIT
-// ==/UserScript==
-
 var location_href = window.location.href;
+
+// ce.esnai.net ===============================================
 if (location_href.indexOf('esnai.net') >= 0){
     if (location_href.indexOf('/c/cpa') >= 0){
         setTimeout(function(){
             homepage_show_detail();
         }, 1000)
     }
+
     if (location_href.indexOf('/showflashvideo') >= 0 ){
         setTimeout(function(){
             setInterval(replay(), 50000);
@@ -28,27 +15,38 @@ if (location_href.indexOf('esnai.net') >= 0){
         }, 1000)
     }
 }
+
+// wangda.chinamobile.com ===============================================
 if (location_href.indexOf('wangda.chinamobile.com/#/study/course/detail/') >= 0){
     setTimeout(function(){
         wangda_chinamobile_com();
     }, 1000)
 }
+
+
+// =======================================================================
+
 function homepage_show_detail(){
     var show_more_button = document.querySelector('button.zk-btn');
     if(show_more_button != null && show_more_button.innerText == '展开更多') {
         show_more_button.click();
     }
 }
+
+
 function replay(){
     clearInterval(tc);
     player.HTML5.play();
     s2j_onVideoPlay();
 };
+
 function showDonloadButton(){
     document.querySelector('#rightfunctions > table:nth-child(4) > tbody > tr:nth-child(2) > td:nth-child(1) > a').click();
+
     setTimeout(function(){
         document.querySelector('body > div:nth-child(14) > div.panel-header.panel-header-noborder.window-header > div.panel-tool > a').click();
     },100)
+
     setTimeout(function(){
         var download_element = document.querySelector('#coursedocswindow > table > tbody > tr:nth-child(2) > td:nth-child(4) > a');
         if (download_element != null || o_judge != undefined || o_single != undefined) {
@@ -57,6 +55,7 @@ function showDonloadButton(){
             tag_body.appendChild(btn_download);
             btn_download.innerText = "下载";
             btn_download.style = "position:fixed;bottom:40%;left:15px;width:60px;height:60px;background:black;opacity:0.75;color:white;text-align:center;line-height:60px;cursor:pointer;";
+
             btn_download.onclick = function(){
                 this.style.display = "none";
                 download_ppt();
@@ -65,6 +64,7 @@ function showDonloadButton(){
         }
     },100)
 }
+
 function download_ppt(){
     var download_elements = document.querySelectorAll('#coursedocswindow > table > tbody > tr');
     var download_element = null;
@@ -72,27 +72,36 @@ function download_ppt(){
     var download_url = '';
     var file_split = [];
     var download_file_extension = '';
+    // var download_name = '';
     var filename_sub = '';
     var filename_pre = document.querySelector('body > div.panel.layout-panel.layout-panel-north > div > table:nth-child(2) > tbody > tr > td:nth-child(1) > span').innerText;
     var filename_final = '';
     for (var i=1; i<download_elements.length; i++) {
         download_element = download_elements[i];
         download_url_element = download_element.querySelector('td:nth-child(4) > a');
+
         if (download_url_element != null) {
             download_url = download_url_element.href;
             file_split = download_url.split(".");
             download_file_extension = file_split[file_split.length - 1];
+
             filename_sub = download_element.querySelector('td:nth-child(1)').innerText;
+
+            // if (filename_sub == '课程讲义' || filename_sub == filename_pre){
             if (filename_sub == filename_pre){
                 filename_final = filename_pre + '.' + download_file_extension;
             }
             else {
                 filename_final = filename_pre + '_' + filename_sub + '.' + download_file_extension;
             }
+
             download(download_url, filename_final)
         }
+        
     }
+
 }
+
 function getBlob(url) {
     return new Promise(resolve => {
         const xhr = new XMLHttpRequest();
@@ -106,6 +115,7 @@ function getBlob(url) {
         xhr.send();
     });
 };
+
 function saveAs(blob, filename) {
     if (window.navigator.msSaveOrOpenBlob) {
         navigator.msSaveBlob(blob, filename);
@@ -122,47 +132,71 @@ function saveAs(blob, filename) {
         window.URL.revokeObjectURL(link.href);
     }
 };
+
 function download(url, filename) {
     getBlob(url).then(blob => {
         saveAs(blob, filename);
     })
 };
+
+
+
 function DownloadQuestionBank(){
+
     var question_bank = '类型,题目,答案';
+
     var question_dict = {};
     var question_info = '';
     var answer = '';
+
     var i=0;
+
+    // 判断题
     var judge_mapping_table = {"A":"对","B":"错"};
     if (o_judge != null){
         for (i=0; i<o_judge.length; i++) {
             question_dict = o_judge[i];
             question_info = question_dict["t"];
             answer = judge_mapping_table[question_dict["a"]];
+
+            // 替换英文逗号，否则保存为csv格式的时候会错乱
             question_info = question_info.replace(',','，').replace('\r\n','；');
             answer = answer.replace(',','，').replace('\r\n','；');
             question_bank = question_bank + '\r\n判断题,' + question_info + ',' + answer;
         }
     }
+
+    // 单选题
     var single_mapping_table = {"A":0,"B":1,"C":2,"D":3,"E":4,"F":5};
     if (o_single != null){
         for (i=0; i<o_single.length; i++) {
             question_dict = o_single[i];
             question_info = question_dict["t"];
             answer = question_dict["o"][single_mapping_table[question_dict["a"]]];
+
+            // 替换英文逗号，否则保存为csv格式的时候会错乱
             question_info = question_info.replace(',','，').replace('\r\n','；');
             answer = answer.replace(',','，').replace('\r\n','；');
             question_bank = question_bank + '\r\n单选题,' + question_info + ',' + answer;
+
         }
     }
+
+    // 正则，批量替换某些字符
     question_bank = question_bank.replace(/&nbsp;/g,'');
     question_bank = question_bank.replace(/&middot;/g,'·');
+    
+    // 导出csv
     if (question_bank.length >= 10 ){
         var download_name_pre = document.querySelector('body > div.panel.layout-panel.layout-panel-north > div > table:nth-child(2) > tbody > tr > td:nth-child(1) > span').innerText;
         var filename = download_name_pre + '_题库';
         convertDataToCsv(question_bank, filename);
+
     }
+    
 };
+
+// 下载
 function convertDataToCsv(data,filename){
     data = "\ufeff" + data;
     let blob = new Blob([data], { type: 'text/csv,charset=UTF-8'});
@@ -171,7 +205,11 @@ function convertDataToCsv(data,filename){
     a.download = filename + ".csv";
     a.href = url;
     a.click();
+
 };
+
+// wangda.chinamobile.com
+
 function wangda_chinamobile_com(){
     var IntervalUnit = 5000;
     var totalIntervalMs = 0;
@@ -182,12 +220,17 @@ function wangda_chinamobile_com(){
         }catch (error) {
             btn = null;
         };
+
         if (btn != null || btn != undefined){
             btn.click();
         };
+
         totalIntervalMs += IntervalUnit;
+
+        // 超过1天，清理24 × 3600 = 86400秒
         if(totalIntervalMs >= 84600000){
             clearInterval(checkInterval);
         }
+
     }, IntervalUnit);
 };
